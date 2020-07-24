c69e98e4ea6d8ef6a102b249216b88fc28b6d74a
ldansorean <9722740+ldansorean@users.noreply.github.com>
2020-07-24 15:31:01 +0100

    Add location updates.

--- app/src/main/java/ldansorean/s6hikerswatch/MainActivity.java
+++ app/src/main/java/ldansorean/s6hikerswatch/MainActivity.java
@@ -10,19 +10,23 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
+    private static final int LOCATION_UPDATE_MIN_TIME = 2000;
+    private static final int LOCATION_UPDATE_MIN_DISTANCE = 1;
     private static final String DETAILS_FORMAT = "Latitude: %.1f \n\r"
                                                 + "Longitude: %.1f \n\r"
-                                                + "Accuracy: %.1f \n\r"
-                                                + "Speed: %.1f \n\r"
+                                                + "Accuracy: %.1f m\n\r"
+                                                + "Speed: %.1f m/s\n\r"
                                                 + "Bearing: %.1f \n\r"
-                                                + "Altitude: %.1f \n\r"
+                                                + "Altitude: %.1f m\n\r"
                                                 + "Address: %s \n\r";
 
     private TextView detailsView;
@@ -37,9 +41,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         setContentView(R.layout.activity_main);
 
         detailsView = findViewById(R.id.details);
-
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         locationProvider = locationManager.getBestProvider(new Criteria(), true);
+        locationManager.requestLocationUpdates(locationProvider, LOCATION_UPDATE_MIN_TIME, LOCATION_UPDATE_MIN_DISTANCE, this);
         geocoder = new Geocoder(getApplicationContext(), Locale.ENGLISH);
 
         Location initialLocation = locationManager.getLastKnownLocation(locationProvider);
@@ -47,19 +51,28 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
     private void showDetails(Location loc) {
-        String address = "";
-        try {
-            address = geocoder.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1).get(0).getAddressLine(0);
-        } catch (IOException e) {
-            ; // not showing any address
+        if (loc != null) {
+            String address = "";
+            try {
+                List<Address> addresses = geocoder.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+                if (addresses != null & addresses.size() > 0) {
+                    Log.i("address", addresses.get(0).getMaxAddressLineIndex() + "");
+                    for (int i = 0; i <= addresses.get(0).getMaxAddressLineIndex(); i++)
+                        address += addresses.get(0).getAddressLine(i) + "\n";
+                }
+            } catch (IOException e) {
+                ; // not showing any address
+            }
+            String details = String.format(DETAILS_FORMAT, loc.getLatitude(), loc.getLongitude(), loc.getAccuracy(), loc.getSpeed(), loc.getBearing(), loc.getAltitude(), address);
+            detailsView.setText(details);
+        } else {
+            detailsView.setText("");
         }
-        String details = String.format(DETAILS_FORMAT, loc.getLatitude(), loc.getLongitude(), loc.getAccuracy(), loc.getSpeed(), loc.getBearing(), loc.getAltitude(), address);
-        detailsView.setText(details);
     }
 
     @Override
     public void onLocationChanged(Location location) {
-            // display location
+         showDetails(location);
     }
 
     @Override
@@ -76,4 +89,19 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     public void onProviderDisabled(String provider) {
 
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(locationProvider, LOCATION_UPDATE_MIN_TIME, LOCATION_UPDATE_MIN_DISTANCE, this);
+    }
+
+
 }
